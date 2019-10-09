d81448c13f346301285ec50c95063fbfdd0dd706
Raymond Nguyen <rayknguyen@gmail.com>
2019-10-09 09:24:01 -0600

    Adds room test file

---
+++ test/room_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+
+class Roomtest < Minitest::Test
+
+  def setup
+  @room = Room.new(:bedroom, 10, 13)
+  end
+
+  def test_it_exist
+    assert_instance_of Room, @room
+  end
+
+  def test_it_has_category
+    assert_equal :bedroom, @room.category
+  end
+
+  def test_it_has_length
+    assert_equal 10, @room.length
+  end
+
+  def test_it_has_width
+    assert_equal 13, @room.width
+  end
+
+  def test_room_has_area
+    assert_equal 130, @room.area
+  end
+end
