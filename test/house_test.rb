e57487c915779c65968a1d53db628cd139aa95c7
Raymond Nguyen <rayknguyen@gmail.com>
2019-10-09 10:54:50 -0600

    House test file pass

--- test/house_test.rb
+++ test/house_test.rb
@@ -41,4 +41,67 @@ class HouseTest < Minitest::Test
     @house.add_room(room_2)
     assert_equal [room_1, room_2], @house.rooms
   end
+
+  def test_it_adds_all_room
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    @house.add_room(room_1)
+    @house.add_room(room_2)
+    @house.add_room(room_3)
+    @house.add_room(room_4)
+    assert_equal [room_1, room_2, room_3, room_4], @house.rooms
+  end
+
+  def test_it_can_find_room_category_bedroom
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    @house.add_room(room_1)
+    @house.add_room(room_2)
+    @house.add_room(room_3)
+    @house.add_room(room_4)
+    @house.rooms_from_category(:bedroom)
+    assert_equal [room_1, room_2], @house.rooms_from_category(:bedroom)
+  end
+
+  def test_it_can_find_room_category_basement
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    @house.add_room(room_1)
+    @house.add_room(room_2)
+    @house.add_room(room_3)
+    @house.add_room(room_4)
+    @house.rooms_from_category(:basement)
+    assert_equal [room_4], @house.rooms_from_category(:basement)
+  end
+
+  def test_it_can_find_room_category_no_category
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    @house.add_room(room_1)
+    @house.add_room(room_2)
+    @house.add_room(room_3)
+    @house.add_room(room_4)
+    @house.rooms_from_category(:loft)
+    assert_equal [], @house.rooms_from_category(:loft)
+  end
+
+  def test_area_of_house
+    room_1 = Room.new(:bedroom, 10, 13)
+    room_2 = Room.new(:bedroom, 11, 15)
+    room_3 = Room.new(:living_room, 25, 15)
+    room_4 = Room.new(:basement, 30, 41)
+    @house.add_room(room_1)
+    @house.add_room(room_2)
+    @house.add_room(room_3)
+    @house.add_room(room_4)
+    assert_equal 1900, @house.area
+  end
 end
